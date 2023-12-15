092bb444b71bc9d47a42e04c4f7737451b64b6f7
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-15 23:10:23 +0100

    styling the perks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,23 @@ import Image from "next/image";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import  Link  from "next/link";
 import { Button, buttonVariants } from "@/components/ui/button";
-import { ArrowDownToLine, CheckCircle } from "lucide-react";
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 
 const perks= [
   {
   name:"Instant Delivery",
   Icon:ArrowDownToLine,
-  description:"Get your assets delivered to your email in seconds and download them right away"
+  description:"Get your assets delivered to your email in seconds and download them right away."
 },
   {
   name:"Guaranteed Quality",
   Icon:CheckCircle,
-  description:"Every asset on our platform is verified by our team to ensure our highest quality standards"
+  description:"Every asset on our platform is verified by our team to ensure our highest quality standards."
 },
   {
-  name:"Instant Delivery",
-  Icon:ArrowDownToLine,
-  description:"Get your assets delivered to your email in seconds and download them right away"
+  name:"For the Plannet ",
+  Icon:Leaf,
+  description:"We've pledged 1% of sales to the preservation and restoration of the natural environment."
 },
 ]
 export default function Home() {
@@ -48,7 +48,16 @@ export default function Home() {
     <section className="border-t border-gray-200 bg-gray-50">
       <MaxWidthWrapper className="py-20">
         <div className="grid grid-cols-1 gap-y-12 sm:gap-x-6 lg:grid-cols-3 lg:gap-cols-3 lg:gap-y-0">
-
+         {perks.map((perk)=>(
+          <div key={perk.name} className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+            <div className="md:flex-shrink-0 flex justify-center">
+              <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
+                {<perk.Icon className="w-1/3 h-1/3"/>}
+              </div>
+            </div>
+            <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6"></div>
+          </div>
+         ))}
         </div>
       </MaxWidthWrapper>
     </section>
