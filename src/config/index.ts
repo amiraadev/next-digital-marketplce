08692819d1d9610bc886079256a25734eefcfc3d
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-15 23:46:46 +0100

    the config folder

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+export const PRODUCTS_CATEGORIES = [
+    {
+        labe:"UI Kits",
+        value:"ui_kits" as const,
+        featured:[
+            {   name:"Editor picks",
+            href:"#",
+            imageSrc:"/nav/ui-kits/mixed.jpg"}
+         
+        ]
+    }
+]
