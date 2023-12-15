3688e42e6df9d4dc6c21fbdf52286d527899135e
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-15 23:53:39 +0100

    adding the pictures

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,22 @@
+/** @format */
+
 export const PRODUCTS_CATEGORIES = [
-    {
-        labe:"UI Kits",
-        value:"ui_kits" as const,
-        featured:[
-            {   name:"Editor picks",
-            href:"#",
-            imageSrc:"/nav/ui-kits/mixed.jpg"}
-         
-        ]
-    }
-]
+	{
+		labe: "UI Kits",
+		value: "icons" as const,
+		featured: [
+			{ name: "Editor picks", href: "#", imageSrc: "/nav/ui-kits/mixed.jpg" },
+			{ name: "New Arrivals", href: "#", imageSrc: "/nav/ui-kits/blue.jpg" },
+			{ name: "Bestsellers", href: "#", imageSrc: "/nav/ui-kits/purple.jpg" },
+		],
+	},
+	{
+		labe: "Icons",
+		value: "ui_kits" as const,
+		featured: [
+			{ name: "Favorite Icon Picks", href: "#", imageSrc: "/nav/icons/bestsellers.jpg" },
+			{ name: "New Arrivals", href: "#", imageSrc: "/nav/icons/new.jpg" },
+			{ name: "Bestsellers", href: "#", imageSrc: "/nav/icons/picks.jpg" },
+		],
+	},
+];
