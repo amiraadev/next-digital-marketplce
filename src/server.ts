e54bf625bcf21dc7b6ca0bded79b20c54192e1f4
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-16 19:53:08 +0100

    setting the middleware for the nextApp

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { getPayloadClient } from './get-payload'
+import { nextApp, nextHandler } from './next-utils'
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
 const start = async () => {
@@ -11,5 +12,11 @@ const start = async () => {
         }
     }
   })
+app.use((req,res) => nextHandler(req,res))
+nextApp.prepare().then(() => {
+    payload.logger.info('Next.js started')
+    app.listen(PORT,async () => {payload.logger.info(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)})
+})
+  
 }
 start()
