a4fc4bb7a03d1e51ad7b3c94d397b8cb010dea17
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-16 19:22:11 +0100

    getting done with the server.ts file

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,13 @@ import { getPayloadClient } from './get-payload'
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
 const start = async () => {
-  const payload = await getPayloadClient()
+  const payload = await getPayloadClient({
+    initOptions:{
+        express:app,
+        onInit:async(cms) => {
+            cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
+        }
+    }
+  })
 }
 start()
