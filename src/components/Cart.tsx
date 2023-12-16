27f7d03f038f30f0260fe3353938d39cc7712137
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-16 14:40:50 +0100

    making the Cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,11 +1,19 @@
 import { ShoppingCart } from "lucide-react"
-import { Sheet, SheetTrigger } from "./ui/sheet"
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet"
 
 const Cart = () => {
     return <Sheet>
-        <SheetTrigger>
+        <SheetTrigger className="group -m-2 flex items-center p-2">
             <ShoppingCart className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"></ShoppingCart>
+            <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800 ">
+                0
+            </span>
         </SheetTrigger>
+        <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+            <SheetHeader className="space-y-2.5 pr-6">
+               <SheetTitle>Cart(0)</SheetTitle>
+            </SheetHeader>
+        </SheetContent>
     </Sheet>
 }
 export default Cart
