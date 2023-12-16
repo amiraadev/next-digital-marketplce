3651e52511dd5d39aa0bad65f0314980df6dd198
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-16 14:49:01 +0100

    adding the separator

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,7 +1,9 @@
-import { ShoppingCart } from "lucide-react"
+import { Divide, ShoppingCart } from "lucide-react"
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet"
+import { Separator } from "@radix-ui/react-separator"
 
 const Cart = () => {
+    const itemcount = 0
     return <Sheet>
         <SheetTrigger className="group -m-2 flex items-center p-2">
             <ShoppingCart className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"></ShoppingCart>
@@ -13,6 +15,20 @@ const Cart = () => {
             <SheetHeader className="space-y-2.5 pr-6">
                <SheetTitle>Cart(0)</SheetTitle>
             </SheetHeader>
+            {
+                itemcount > 0 ? (
+                <>
+                 <div className="flex w-full flex-col pr-6">
+                    {/* TO DO */}
+                    cart items
+                 </div>
+                 <div className="space-y-4 pr-6">
+                    <Separator/>
+                    <div className="space-y-1 5 pr-6"></div>
+                 </div>
+                </>
+                ):(<div></div>)
+            }
         </SheetContent>
     </Sheet>
 }
