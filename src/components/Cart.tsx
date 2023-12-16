3d75075577f9907611c566198ad45d726f263859
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-16 14:30:49 +0100

    shadcn-ui@latest add sheet

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,11 @@
+import { ShoppingCart } from "lucide-react"
+import { Sheet, SheetTrigger } from "./ui/sheet"
+
 const Cart = () => {
-    return()
+    return <Sheet>
+        <SheetTrigger>
+            <ShoppingCart className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"></ShoppingCart>
+        </SheetTrigger>
+    </Sheet>
 }
 export default Cart
