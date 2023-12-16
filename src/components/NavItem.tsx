f76fe1023a29b4a0da6e035f52e51960eb9168bd
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-16 13:21:45 +0100

    showing the navITems with their images

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -5,7 +5,9 @@ import { PRODUCTS_CATEGORIES } from "@/config";
 import { Button } from "./ui/button";
 import { ChevronDown } from "lucide-react";
 import { cn } from "@/lib/utils";
-type Category = typeof PRODUCTS_CATEGORIES[number];
+import Image from "next/image";
+import Link from "next/link";
+type Category = (typeof PRODUCTS_CATEGORIES)[number];
 interface NavItemProps {
 	category: Category;
 	handleOpen: () => void;
@@ -14,11 +16,69 @@ interface NavItemProps {
 }
 const NavItem = ({ category, handleOpen, isOpen, isAnyOpen }: NavItemProps) => {
 	return (
-		<div className='relative flex items-center'>
-			<Button className='gap-1.5' onClick={handleOpen} variant={isOpen? "secondary" :"ghost"}>{category.label}<ChevronDown className={cn("h-4 w-4 transition-all text-muted-foreground",{
-                "-rotate-180" :isOpen
-            })}/></Button>
-		</div>
+		<div className='flex'>
+			<div className='relative flex items-center'>
+				<Button
+					className='gap-1.5'
+					onClick={handleOpen}
+					variant={isOpen ? "secondary" : "ghost"}>
+					{category.label}
+					<ChevronDown
+						className={cn("h-4 w-4 transition-all text-muted-foreground", {
+							"-rotate-180": isOpen,
+						})}
+					/>
+				</Button>
+			</div>
+
+			{isOpen ? (
+				<div
+					className={cn(
+						"absolute inset-x-0 top-full text-sm text-muted-foreground",
+						{ "animate-in fade-in-10 slide-in-from-top-5": !isAnyOpen }
+					)}>
+					<div
+						className='absolute inset-0 top-1/2 bg-white shadow'
+						aria-hidden='true'
+					/>
+
+<div className='relative bg-white'>
+            <div className='mx-auto max-w-7xl px-8'>
+              <div className='grid grid-cols-4 gap-x-8 gap-y-10 py-16'>
+                <div className='col-span-4 col-start-1 grid grid-cols-3 gap-x-8'>
+                  {category.featured.map((item) => (
+                    <div
+                      onClick={() => close}
+                      key={item.name}
+                      className='group relative text-base sm:text-sm'>
+                      <div className='relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'>
+                        <Image
+                          src={item.imageSrc}
+                          alt='product category image'
+                          fill
+                          className='object-cover object-center'
+                        />
+                      </div>
+
+                      <Link
+                        href={item.href}
+                        className='mt-6 block font-medium text-gray-900'>
+                        {item.name}
+                      </Link>
+                      <p
+                        className='mt-1'
+                        aria-hidden='true'>
+                        Shop now
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
 	);
 };
 export default NavItem;
